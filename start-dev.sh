fda9ef08c65e5162983f20cb44142ec088f2dfea
LevantinLynx <git@levantinlynx.com>
2025-05-10 00:54:37 +0000

    updated dev script

--- start-dev.sh
+++ start-dev.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 docker build -f Dockerfile-dev -t mediathek-movie-downloader:dev .
 
-docker run --rm -it -v $(pwd):/usr/src/app -v $(pwd)/downloads:/usr/src/app/downloads -e TZ=Europe/Berlin -e NODE_ENV=development -p 12345:12345 mediathek-movie-downloader:dev /bin/sh
+docker run --rm -it -v $(pwd):/usr/src/app \
+  -e TZ=Europe/Berlin \
+  -e NODE_ENV=development \
+  -p 12345:12345 \
+  mediathek-movie-downloader:dev /bin/sh
