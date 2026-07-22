LevantinLynx/mediathek-movie-downloader

commit a2a0ad4098e739643c1a6e9e95e1a5badecb1679

channel ordering, channel logo size changes

client/src/stores/movie.js
@@ -28,9 +28,18 @@ export const useMovieStore = defineStore('movie', {
       if (!state.availableMovieMetaData) return []
 
       let sortedList = Object.values(state.availableMovieMetaData)
+      const channelOrder = [
+        'zdf', 'zdfneo', 'zdftivi',
+        'arte', '3sta',
+        'ard', 'das_erste', 'one', 'ard_alpha',
+        'ndr', 'mdr', 'wdr', 'swr', 'rbb',
+        'hr', 'br', 'sr',
+        'funk', 'kika'
+      ]
       const filterIds = [ ...state.ignoredMovieIds, ...state.doneMovieIds, ...state.scheduledIds ]
       sortedList = sortedList.filter(movie => filterIds.indexOf(movie.id) === -1)
-      sortedList = _.compact(_.uniq(sortedList.map(movie => movie.channel)).sort())
+      sortedList = _.compact(_.uniq(sortedList.map(movie => movie.channel)))
+      sortedList = _.intersection(channelOrder, sortedList)
       return sortedList
     },
     availableMovies (state) {

client/src/components/Menu.vue
@@ -1,11 +1,11 @@
 <template>
   <aside class="menu" :class="{ pwaMode: settingsStore.isLaunchedAsApp, ios: settingsStore.isIosStandalone }">
-    <transition>
+    <Transition>
     <aside id="connectionStatus" v-if="!state.connected">
       <IconShieldFail />
       <span>Keine Verbindung zum Server.</span>
     </aside>
-    </transition>
+    </Transition>
     <div id="menu">
       <nav>
         <RouterLink to="/upcoming" aria-label="Verfügbare Filme" :class="{ 'active': upcomingRouteSelected }">
@@ -99,9 +99,6 @@ const settingsRouteSelected = computed(() => route.fullPath.indexOf('/settings')
   --icon-size: 5rem;
   width: var(--icon-size);
   height: var(--icon-size);
-}
-#subMenu {
-  padding-bottom: 1rem;
 }
 
 #connectionStatus {

client/src/views/Completed.vue
@@ -40,7 +40,7 @@
               <div class="time">{{ formatDate(movie.date) }}</div>
               <div class="split">
                 <img :src="'/logo/' + movie.channel + '.svg'"
-                  :height="movie.channel === 'arte' ? '16px' : '20px'"
+                  :height="['arte', 'ard', 'zdf'].indexOf(movie.channel) > -1 ? '16px' : '20px'"
                   :alt="movie.channel + ' Logo'"
                   loading="lazy"
                   v-if="movie.channel">
@@ -117,7 +117,7 @@
               <div class="time">{{ formatDate(movie.date) }}</div>
               <div class="split">
                 <img :src="'/logo/' + movie.channel + '.svg'"
-                  :height="movie.channel === 'arte' ? '16px' : '20px'"
+                  :height="['arte', 'ard', 'zdf'].indexOf(movie.channel) > -1 ? '16px' : '20px'"
                   :alt="movie.channel + ' Logo'"
                   loading="lazy"
                   v-if="movie.channel">

client/src/views/Schedule.vue
@@ -94,7 +94,7 @@
           <div style="flex: 1;"></div>
           <div class="action">
             <img :src="'/logo/' + movie.channel + '.svg'"
-              :height="movie.channel === 'arte' ? '16px' : '22px'"
+              :height="['arte', 'ard', 'zdf'].indexOf(movie.channel) > -1 ? '16px' : '22px'"
               :alt="movie.channel + ' Logo'"
               loading="lazy"
               v-if="movie.channel">

client/src/views/Upcoming.vue
@@ -29,7 +29,7 @@
           </transition>
           <a @click="preFilter === channel ? preFilter = '' : preFilter = channel" v-for="channel in channelList" :class="{ active: preFilter === channel }">
             <img :src="'/logo/' + channel + '.svg'"
-              :height="channel === 'arte' ? '16px' : '22px'"
+              :height="['arte', 'ard', 'zdf'].indexOf(channel) > -1 ? '16px' : '22px'"
               :alt="channel + ' Logo'"
               loading="lazy">
           </a>

client/src/views/UpcomingMovie.vue
@@ -26,7 +26,7 @@
       <div class="info box">
         <header>
           <img :src="'/logo/' + movie.channel + '.svg'"
-            :height="movie.channel === 'arte' ? '16px' : '22px'"
+            :height="['arte', 'ard', 'zdf'].indexOf(movie.channel) > -1 ? '16px' : '22px'"
             :alt="movie.channel + ' Logo'"
             loading="lazy">
           <p>{{ movie.preText }}</p>

commit 6b9c032e498e56646c7461f2356557c6b629a84b

fixed typo regression from a2a0ad40

client/src/components/Changelog.vue
@@ -7,6 +7,11 @@
     <h2>Changelog</h2>
 
     <div class="scrollWrapper">
+      <h4>2026.7.22</h4>
+      <ul>
+        <li>3sat taucht nun wieder im Filter der Übersicht auf.</li>
+      </ul>
+
       <h4>2026.6.27</h4>
       <ul>
         <li>Änderungen an der 3sat API übernommen.</li>

client/src/stores/movie.js
@@ -30,7 +30,7 @@ export const useMovieStore = defineStore('movie', {
       let sortedList = Object.values(state.availableMovieMetaData)
       const channelOrder = [
         'zdf', 'zdfneo', 'zdftivi',
-        'arte', '3sta',
+        'arte', '3sat',
         'ard', 'das_erste', 'one', 'ard_alpha',
         'ndr', 'mdr', 'wdr', 'swr', 'rbb',
         'hr', 'br', 'sr',